313dce842b6025527d90c51f79539ac64e198797
nael.lilik <nael.lilik@gmail.com>
2024-07-18 15:38:25 +0700

    yang bener ini

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const axios = require('axios');
-const morgan = require('morgan');
+const fetch = require('node-fetch');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware untuk logging menggunakan morgan (default: combined)
-app.use(morgan('dev')); // Gunakan 'dev' untuk format log yang lebih ringkas
-
-// Middleware untuk mengizinkan CORS
+// Middleware untuk mengizinkan CORS (jika diperlukan)
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -21,12 +16,10 @@ app.get('/api/get-coordinates', async (req, res) => {
   const { url } = req.query;
 
   try {
-    // Mengirim permintaan untuk mengambil URL pendek dari Google Maps
-    const response = await axios.get(url);
-    if (response.status === 200) {
-      const { data } = response;
-      // Dari data, kita bisa ambil koordinatnya
-      const match = data.match(/@(-?\d+\.\d+),(-?\d+\.\d+)/);
+    const response = await fetch(url, { redirect: 'manual' });
+    if (response.ok) {
+      const finalUrl = response.url;
+      const match = finalUrl.match(/@(-?\d+\.\d+),(-?\d+\.\d+)/);
 
       if (match) {
         const lat = parseFloat(match[1]);
@@ -36,15 +29,16 @@ app.get('/api/get-coordinates', async (req, res) => {
         res.status(400).json({ success: false, message: 'URL tidak valid atau tidak mengandung koordinat.' });
       }
     } else {
-      res.status(response.status).json({ success: false, message: 'Gagal mengambil URL akhir dari Google Maps.' });
+      res.status(response.status).json({ success: false, message: 'Gagal mengambil URL akhir.' });
     }
   } catch (error) {
-    console.error('Error fetching URL:', error);
+    console.error('Error fetching final URL:', error);
     res.status(500).json({ success: false, message: 'Terjadi kesalahan saat mengambil URL akhir.' });
   }
 });
 
-// Menjalankan server
+// Jalankan server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
